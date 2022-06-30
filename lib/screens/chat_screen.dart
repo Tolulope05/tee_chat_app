0e41b88f87a273d26f0fa78d772c656b390c78c5
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-06-30 10:11:26 +0100

    androids and push  notificatiuons [RECIEVED MESSAGE SUCCESS]

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    messaging.getInitialMessage();
+    messaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    messaging.getToken().then((token) {
+      print(token);
+    });
+    FirebaseMessaging.onMessage.listen((message) {
+      print(message);
+      print(message.notification!.body);
+      print(message.notification!.title);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
